e81d06cac1a8bd723e5aab3a0aceaac97efc177d
ronan <ronan.lachater@gmail.com>
2017-04-19 19:37:25 +0200

    commit test isShipping

--- src/test/java/fr/afcepf/atod/ws/soap/shipping/ShippingTest.java
+++ src/test/java/fr/afcepf/atod/ws/soap/shipping/ShippingTest.java
@@ -6,10 +6,15 @@ import fr.afcepf.atod.ws.soap.shipping.util.RESTUtil;
 
 public class ShippingTest {
 	private SoapShippingService service = new SoapShippingService();
-	
+	private static final int KEY_ADDRESS = 1;
 
 	@Test(expected = Exception.class)
 	public void testNominal() throws Exception {
 		service.orderShipping(null);
 	}	
+	
+	@Test
+	public void testIsShippingNominalCase() throws Exception {
+		service.getIdShippingOrderFromApp(String.valueOf(KEY_ADDRESS));
+	}
 }
